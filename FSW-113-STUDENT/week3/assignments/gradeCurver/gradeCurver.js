2904a27ecadd77c5b6d1010baa906f784483ea7e
Ali Al Dairawi <ali.aldairawi@bryanuniversity.edu>
2022-04-08 18:00:41 -0700

    Uploading Star Wars Data Checker Assignment

--- FSW-113-STUDENT/week3/assignments/gradeCurver/gradeCurver.js
+++ FSW-113-STUDENT/week3/assignments/gradeCurver/gradeCurver.js
@@ -4,9 +4,16 @@
 
 // create an event listener that resets the scores and grades to their defaults when the Reset button is clicked
 
-function applyBell(grade, index, ary) {
+const submitBtn = document.getElementById("submit");
+let range = maxGrade - minGrade;
+
+const  applyBell = (grade, index, ary) => {
+    
+    
+    let gradeSlice = range / 5;
     switch (true) {
         case grade >= (mean + (gradeSlice * 2)): 
+            console.log(grade);
             ary[index] = 'A'
             break
         case grade >= (mean + gradeSlice): 
@@ -24,45 +31,39 @@ function applyBell(grade, index, ary) {
     }
 }
 
-function convertArray(obj) {
-    ary = obj.value.split(',')
-    ary = ary.map(function (x) {
-        return parseInt(x)
-    })
+const  convertArray = (obj) => {
+    let ary = obj.value.split(',').map( x => parseInt(x));
     return ary
 }
+
 
 // Condense the number of lines within the curveGrade() function as much as possible by converting 
 // the functions to arrow functions. You can also condense the number of lines by combining some 
 // separate lines of code into single lines. It currently has 18 lines of code. Without counting  
 // empty lines, can you get the number of lines down to 8?
 
-function curveGrades() {
-    **sum = function (accumulator, currentValue) {
-        return accumulator + currentValue
-    }
 
-    **sumGrades = function(array) {
-        return array.reduce(sum)
-    }
+const showGrades = document.querySelector("#grades");
+const resetGrades = document.querySelector("#reset");
 
-    **aryGrades = convertArray(document.querySelector('#scores'))
 
-    **minGrade = aryGrades.reduce(function(a, b) {
-        return Math.min(a, b)
-    })
+const curveGrades = () => {
     
-    **maxGrade = aryGrades.reduce(function(a, b) {
-        return Math.max(a, b)
-    })
-    
-    **mean = sumGrades(aryGrades) / aryGrades.length
+    const aryGrades = convertArray(document.querySelector("#scores"));
+    console.log(aryGrades);
 
-    **range = maxGrade - minGrade
+    const minGrade = aryGrades.reduce((a, b) => Math.min(a,b));
+    const maxGrade = aryGrades.reduce((a, b) => Math.max(a,b)); 
 
-    gradeSlice = range / 5
+    const sum = (accumulator, currentValue) => accumulator + currentValue;
+    const sumGrades = (array) => array.reduce(sum);
+    let mean = sumGrades(aryGrades) / aryGrades.length;
 
-    aryGrades.forEach(applyBell)
-
+    console.log("Transformed array printing...");
     // write the value of aryGrades to the grades div in the HTML document
-}
+    showGrades.innerText = `${aryGrades.forEach(applyBell)}`;
+};
+
+submitBtn.addEventListener("click", curveGrades);
+resetGrades.addEventListener("click", () => console.log('Hello World'));
+
--- FSW-113-STUDENT/week3/assignments/starwars/starwars.js
+++ FSW-113-STUDENT/week3/assignments/starwars/starwars.js
@@ -1,25 +1,61 @@
 // declare any necessary variables
 
+
+
 // define a function called 'fetchData()' that passes the values from 
 // the 'queryType' and 'itemID' elements in starwars.html to the function 
 // called 'getFromSWAPI()'
+const queryTypeElement = document.getElementById("queryType");
+const fetchData = () => {
+    
+    let itemIDElement = document.querySelector("#itemID");
+    if(itemIDElement.value < 1 || itemIDElement.value > 10) {
+        alert("Please enter a valid positive ID Between 1 And 10!");
+        itemIDElement.value = "";
+        return;
+    }
+    getFromSWAPI(queryTypeElement.value, itemIDElement.value);
 
-function getFromSWAPI() {
+}
+
+
+const  getFromSWAPI = (queryType, itemID) => {
     // assign values to any necessary variables
-    fetch(`https://swapi.dev/api/${queryType}/${itemID}`)
-    .then(function (response) {
-        return response.json()
+    const nameDomElement = document.getElementById("dataLabel1");
+    const nameDomValue = document.getElementById("dataValue1");
+    const descDomElement = document.getElementById("dataLabel2");
+    const descDomValue = document.getElementById("dataValue2");
+
+    fetch(`https://swapi.dev/api/${queryType}`)
+    .then((response) => {
+        return response.json();
     })
-    .then(function(data){
-        updateInfo(data)
+    .then((data) => {
+
+        
+        updateInfo(data,itemID,nameDomElement, nameDomValue, descDomElement, descDomValue);
     })
     .catch(function(err) {
-        console.warn(err)
+        console.warn(err);
     })
 }
+
 
 // create a new function called 'updateInfo()' that receives the data from 
 // the call to that function (see above). Use logic to write the appropriate
 //labels to 'dataLabel1' and 'dataLabel2' elements in starwars.html, as well
 // as the appropriate values from the data object to the 'dataValue1' and 
 // 'dataValue2' elements in starwars.html.
+
+const updateInfo = (dataToOutput,dataIndex, nameEl, nameVal, descEl, descVal ) => {
+    
+    const resultArray = {...dataToOutput.results[dataIndex - 1]};
+    let keyPair = Object.keys(resultArray);
+    let valuePair = Object.values(resultArray);
+    let dynamicString = queryTypeElement.value === "people" ? "Person": queryTypeElement.value;
+    nameEl.innerText = `${dynamicString} ${keyPair[0].replace("_", " ")}:`;
+    nameVal.innerText = `${valuePair[0].replace("_", " ")}`;
+    descEl.innerText = `${keyPair[3].replace("_", " ")}:`;
+    descVal.innerText = `${valuePair[3].replace("_", " ")}`;
+};
+
