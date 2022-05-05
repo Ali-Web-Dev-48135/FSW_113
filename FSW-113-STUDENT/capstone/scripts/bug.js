669b5b40776747ef155c64c9a9689d89f9d508ee
Ali Al Dairawi <ali.aldairawi@bryanuniversity.edu>
2022-05-04 17:01:21 -0700

    Bug_Tracker_Capstone_Project_FSW-113_Week_7

--- FSW-113-STUDENT/capstone/scripts/bug.js
+++ FSW-113-STUDENT/capstone/scripts/bug.js
@@ -1,28 +1,83 @@
 // Create a variable of the right kind and in the right place such that each new bug that is added can increment that number
 
 class Bug {
-    constructor() {
+    constructor(reportedBy, system, subSystem, bugDesc) {
         // This constructor should be set up to accept the four user-input values from index.html: 
         // reportedBy, system, subSystem, and bugDesc
+        this.reportedBy = reportedBy;
+        this.system = system;
+        this.subSystem = subSystem;
+        this.bugDesc = bugDesc;
     }
 
     addBug() {
         // Create a div element that displays the bug information input by the user within the "listWrapper" DOM element. 
         // It should also contain buttons whose onClick events will call the deleteBug() and resolveBug() methods (see below). 
-
+        let parentBugContainer = document.querySelector("#listWrapper");
+        let bugContainerDomElement = document.createElement("div");
+        bugContainerDomElement.className = "bugContainer";
+        // bugContainerDomElement.id = this.instanceCounter;
+        bugContainerDomElement.innerHTML = `
+            <h5>Reported By: ${this.reportedBy}</h5>
+            <h5>System: ${this.system}/${this.subSystem}</h5>
+            <p>${this.bugDesc}</p>
+           <button class="deleteBtn">Delete &cross;</button>
+           <button class="doneBtn">Done &check;</button>
+        `;
+        let buttonDomElements = bugContainerDomElement.querySelectorAll("button");
+        buttonDomElements[0].addEventListener("click", this.deleteBug.bind(null, bugContainerDomElement));
+        buttonDomElements[1].addEventListener("click", this.resolveBug.bind(null, bugContainerDomElement));
+        parentBugContainer.append(bugContainerDomElement);
+        
     }
 
-    deleteBug() {
+    deleteBug(event) {
         // Create code that will remove the appropriate bug from the DOM. 
         // You may need to Google how to remove an element from the DOM.
+        event.remove();
     }
 
-    resolveBug() {
+    resolveBug(event) {
         // Create code that changes the appropriate bug report to a darker color
+        // continure from here maybe add a new css style to background color.
+        // adding the class to change the background color for the completed bug.
+        event.classList.add("bugCompleteStyle");
+        
     }
 }
 
-function reportBug() {
+function reportBug(e) 
+{
+    e.preventDefault();
     // Create code that instantiates the Bug class with the data input by the 
     // user in the index.html form. Then call the method to add the new bug report.
+    let reportedByValue = document.querySelector("#reportedBy").value;
+    let systemValue = document.querySelector("#system").value;
+    let subSystemValue = document.querySelector("#subSystem").value;
+    let bugDescValue = document.querySelector("#bugDesc").value;
+
+    // validating user input.
+    if(reportedByValue === "" || subSystemValue === "" ||
+       systemValue === "" || bugDescValue === ""
+    ){
+        alert("Please Enter All The Required Fields.");
+        return;
+    }
+    let newBug = new Bug(reportedByValue, systemValue, subSystemValue, bugDescValue);
+    newBug.addBug();
+
+
 }
+
+const resetFields = () =>
+{
+    let reportedByValue = document.querySelector("#reportedBy").value = "";
+    let systemValue = document.querySelector("#system").value = "";
+    let subSystemValue = document.querySelector("#subSystem").innerHTML = "";
+    let bugDescValue = document.querySelector("#bugDesc").value = "";
+
+}
+
+document.querySelector("#submitBtn").addEventListener("click", reportBug);
+document.querySelector("#resetBtn").addEventListener("click", resetFields);
+
--- FSW-113-STUDENT/capstone/scripts/data.js
+++ FSW-113-STUDENT/capstone/scripts/data.js
@@ -1,7 +1,9 @@
 // Call the "getSystems()" function in such a way that when the page loads, the "system" select element displays the three sytems 
 // whose parentID is zero.
 
-function getSystems() {
+
+
+async function getSystems(parentId, DomELement) {
     // This function will accept two arguments (see week three): the parentID and the DOM element that will ultimately receive the data. 
     // Code this function as an asynchronous operation that will fetch data from the data.json file (see week three).
     // After receiving the JSON data, the asynchronous promise should use a higher order array method (see week two) to return only 
@@ -10,17 +12,83 @@ function getSystems() {
 
     // Note that although a number being passed as a parameter into a function may look like a numeral, it may be a string value 
     // instead, and may need to be converted to an integer. 
+
+    let response = await fetch("./data.json")
+    .then(async response => {
+        const responseData = await response.json();
+        const filteredParentIdArray = [responseData.systems.find(element => element.sysName === parentId)];
+        const formedArrayFromParentId = [responseData.systems.filter(element => element.parentID === filteredParentIdArray[0].sysID)];
+        
+        // passing the filtered array to populateDD function.
+        populateDD(formedArrayFromParentId, DomELement);
+    })
+    .catch(error => console.log(error));
+
 }
 
-function populateDD() {
+function populateDD(systemsJsonArray, DomElement) {
     // This function receives the array and DOM element from the "getSystems()" function (above). This function should fill the 
     // appropriate DOM element with options from which the user can select. However, since that DOM element has an "onChange" event, this
     // function first needs to add an option that says "Select an Item". Then use a looping mechanism (week one) to create the rest of  
     // the select element's options using the sysName and sysID from the passed in array (see week five).
+    
+    let childNodeArray = [...DomElement.childNodes];
+    for(let i = 0; i < childNodeArray.length; i++)
+    {
+        DomElement.removeChild(childNodeArray[i]);
+    }
+    // creating a new array with the proper structure to loop over and populate the #subSystem DomElement.
+
+    const DESTRUCTED_ARRAY = [...systemsJsonArray[0]];
+    DESTRUCTED_ARRAY.forEach(element => {
+        let optionDomElement = document.createElement("option");
+        optionDomElement.value = element.sysName;
+        optionDomElement.innerText = `${element.sysName}`;
+        DomElement.append(optionDomElement);
+    });
 }
 
-document.querySelector('#system').addEventListener(
-    // This eventListener responds to a change to the "system" select element by passing the selected value from the "system" element 
+document.querySelector('#system').addEventListener("change", () => {
+    //This eventListener responds to a change to the "system" select element by passing the selected value from the "system" element 
     // to the "getSystems()" function along with the "subSystem" DOM element so that the second drop-down list is populated with the 
     // appropriate sub-systems from the data.json file.
-)
+     const SUB_SYSTEM_DOM_ELEMENT = document.querySelector("#subSystem");
+     const DEFAULT_VALUE = "defaultVal";
+     let systemValue = document.querySelector("#system").value;
+     if(systemValue === DEFAULT_VALUE)
+     {
+        alert("Select one of the following to filter, Operations , Sales or Customer Support");
+        return;
+     }
+     getSystems(systemValue, SUB_SYSTEM_DOM_ELEMENT);
+})
+
+
+
+// populate the system select DomElement.
+
+const populateSystemFunction =  async () => {
+    // querying the #system dom element and appending the default option.
+    const SYSTEM_SELECT_DOM_ELEMENT = document.querySelector("#system");
+    const DEFAULT_OPTION_VALUE = document.createElement("option");
+    DEFAULT_OPTION_VALUE.value = "defaultVal";
+    DEFAULT_OPTION_VALUE.innerText = "Select An Item";
+    SYSTEM_SELECT_DOM_ELEMENT.append(DEFAULT_OPTION_VALUE);
+
+    // fetching and filtering the elements where parent ID is zero.
+    const dataJsonResponse = await fetch("./data.json");
+    const responseParsed = await dataJsonResponse.json();
+    const filteredParentIdZeroArray = [...responseParsed.systems.filter(element => element.parentID === 0)];
+
+    // looping over the ParentID zero object array and creating options elements for each one.
+    filteredParentIdZeroArray.forEach(element => {
+        let newOption = document.createElement("option");
+        newOption.value = element.sysName;
+        newOption.innerText = element.sysName;
+        SYSTEM_SELECT_DOM_ELEMENT.append(newOption);
+    });
+};
+
+populateSystemFunction();
+
+
